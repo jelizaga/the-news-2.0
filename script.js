b4169d1ab1bde634c3f8340cf7bbc539beacd787
Joel Elizaga <joelizaga@hotmail.com>
2016-01-29 21:37:07 -0800

    Buttons added.

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+	alert("test");
+	getSubtitle();
+
+)};
+
+/*
+var getSubtitle = function() {
+
+	var subtitlePicker = Math.random() + 10;
+	alert(subtitlePicker);
+
+}
+*/
